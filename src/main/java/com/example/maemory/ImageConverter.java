cb3d9749a1ad2fa468124e3883a26ab0e5004c97
Marcit2 <marcitrenki@gmail.com>
2022-05-17 09:21:07 +0200

    addes Files

---
+++ src/main/java/com/example/maemory/ImageConverter.java
@@ -0,0 +1,75 @@
+package com.example.maemory;
+
+import javafx.scene.image.*;
+import javafx.stage.*;
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class ImageConverter {
+	
+	public static ArrayList<Spielkarte> convertWithDialog(int width, int height) throws Exception{
+		
+		DirectoryChooser chooser = new DirectoryChooser();
+		chooser.setTitle("choose directory with images");
+		File file = chooser.showDialog(null);
+		
+		if (!file.isDirectory()) {
+			
+			throw new IllegalArgumentException("you must select a directory");
+			
+		}else {
+			
+			return convertWithoutDialog(width,height,file.listFiles());
+			
+		}
+		
+	}
+	
+	public static ArrayList<Spielkarte> convertWithoutDialog (int width, int height, File... files) throws Exception {
+		
+		ArrayList<Spielkarte> spielkartes = new ArrayList<>();
+		ArrayList<Image> images = new ArrayList<>();
+		final Image[] background = {null};
+		final Exception[] ex = {null};
+		
+		if (Stream.of(Objects.requireNonNull(files)).count() == 17 || Stream.of(Objects.requireNonNull(files)).count() > 17) {
+			
+			Stream.of(Objects.requireNonNull(files)).forEach((f) -> {
+				
+				try {
+					
+					if (!f.getName().contains("background")) {
+						
+						images.add(new Image(f.getPath(),width,height,false,false)/*new Image(new FileInputStream(f))*/);
+						
+					}else {
+						
+						background[0] = new Image(f.getPath(),width,height,false,false)/*new Image(new FileInputStream(f))*/;
+						
+					}
+					
+				} catch (Exception e) {
+						ex[0] = e;
+				}
+				
+			});
+			
+			if (ex[0] != null) {
+				throw ex[0];
+			}
+			
+			images.forEach((i) -> spielkartes.add(new Spielkarte(i,background[0])));
+			
+			}else {
+				
+				throw new IllegalArgumentException("there must be at least 17 pictures");
+				
+			}
+		
+		return spielkartes;
+		
+		
+	}
+	
+}
