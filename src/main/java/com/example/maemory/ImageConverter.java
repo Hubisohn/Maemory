60377d8e97168508a84e816bd8f3016bc9f54bfb
Marcit2 <marcitrenki@gmail.com>
2022-05-17 11:10:15 +0200

    changed ImageConverter.java

--- src/main/java/com/example/maemory/ImageConverter.java
+++ src/main/java/com/example/maemory/ImageConverter.java
@@ -1,10 +1,12 @@
 package com.example.spielkartensystem;
 
+import javafx.fxml.*;
+import javafx.scene.control.*;
 import javafx.scene.image.*;
 import javafx.stage.*;
 import org.jetbrains.annotations.*;
-
 import java.io.*;
+import java.nio.file.spi.*;
 import java.util.*;
 import java.util.stream.*;
 
@@ -18,6 +20,9 @@ public class ImageConverter {
 		
 		if (!file.isDirectory()) {
 			
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setContentText("you must select a directory");
+			alert.show();
 			throw new IllegalArgumentException("you must select a directory");
 			
 		}else {
@@ -31,6 +36,9 @@ public class ImageConverter {
 	public static ArrayList<Spielkarte> convertWithoutDialog (@NotNull Integer width,@NotNull Integer height, @NotNull File... files) throws Exception {
 		
 		if (width <= 0 || height <= 0) {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setContentText("width and height must be greater than 0");
+			alert.show();
 			throw new IllegalArgumentException("width and height must be greater than 0");
 		}
 		
@@ -54,28 +62,40 @@ public class ImageConverter {
 				
 				try {
 					
+					Image image = new Image(f.getPath(),width,height,false,false);
+					
+					if (image.isError()) {
+						throw new LoadException("File is not of type image");
+					}
+					
 					if ((!f.getName().contains("background") || !f.getName().contains("back")) && background[0] != null) {
 						
-						images.add(new Image(f.getPath(),width,height,false,false)/*new Image(new FileInputStream(f))*/);
+						images.add(image/*new Image(new FileInputStream(f))*/);
 						
 					}else {
 						
-						background[0] = new Image(f.getPath(),width,height,false,false)/*new Image(new FileInputStream(f))*/;
+						background[0] = image/*new Image(new FileInputStream(f))*/;
 						
 					}
 					
 				} catch (Exception e) {
-						ex[0] = e;
+					ex[0] = e;
 				}
 				
 			});
 			
 			if (ex[0] != null) {
+				Alert alert = new Alert(Alert.AlertType.ERROR);
+				alert.setContentText(ex[0].getMessage());
+				alert.show();
 				throw ex[0];
 			}
 			
 			if (background[0] == null) {
 				
+				Alert alert = new Alert(Alert.AlertType.ERROR);
+				alert.setContentText("there has to be at least one picture to be used as a back for the cards, this picture must contain \"back\" or \"background\" in its filename");
+				alert.show();
 				throw new IllegalStateException("there has to be at least one picture to be used as a back for the cards, this picture must contain \"back\" or \"background\" in its filename");
 				
 			}
@@ -83,8 +103,11 @@ public class ImageConverter {
 			images.forEach((i) -> spielkartes.add(new Spielkarte(i,background[0])));
 			
 			}else {
-				
-				throw new IllegalArgumentException("the amount of pictures has to be equal to a power of 2 + 1");
+			
+				Alert alert = new Alert(Alert.AlertType.ERROR);
+				alert.setContentText("the amount of pictures has to be equal to a power of 2 + 1");
+				alert.show();
+				throw new IllegalStateException("the amount of pictures has to be equal to a power of 2 + 1");
 				
 			}
 		
