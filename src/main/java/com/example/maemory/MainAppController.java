caf6603929ae3e979a8e0b3e8687c690cb17beac
Marcit2 <marcitrenki@gmail.com>
2022-05-18 15:22:25 +0200

    changed Files

--- src/main/java/com/example/maemory/MainAppController.java
+++ src/main/java/com/example/maemory/MainAppController.java
@@ -3,13 +3,15 @@ package com.example.maemory;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.util.*;
+
 public class MainAppController {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() throws Exception {
-        ImageConverter.convertWithDialog(100,100);
+        ArrayList<Spielkarte> list = ImageConverter.convertWithDialog(100,100);
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
