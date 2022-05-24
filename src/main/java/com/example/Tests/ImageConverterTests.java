75c14e5dde220e18e196c23dd46f232c11f0dda8
Marcit2 <marcitrenki@gmail.com>
2022-05-24 09:55:03 +0200

    edited Files

--- src/main/java/com/example/Tests/ImageConverterTests.java
+++ src/main/java/com/example/Tests/ImageConverterTests.java
@@ -1,16 +1,22 @@
 package com.example.Tests;
-
 import com.example.maemory.*;
+import javafx.beans.*;
+import javafx.beans.property.*;
+import javafx.beans.value.*;
+import javafx.collections.*;
+import javafx.scene.control.*;
+import javafx.util.*;
 import org.junit.jupiter.api.*;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ImageConverterTests {
 	
+	
+	
 		@Test
 		private void convertImageNegativeSize () throws Exception {
-			
+
 			assertThrows(IllegalArgumentException.class,() -> ImageConverter.convertWithoutDialog(-1,-1,null));
 			
 		}
