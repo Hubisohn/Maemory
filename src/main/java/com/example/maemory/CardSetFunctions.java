0656c76c1b808f408e90b426970fc233ec3a7520
Marcit2 <marcitrenki@gmail.com>
2022-05-25 14:23:14 +0200

    edited Files

--- src/main/java/com/example/maemory/CardSetFunctions.java
+++ src/main/java/com/example/maemory/CardSetFunctions.java
@@ -212,11 +212,11 @@ public class CardSetFunctions {
 		Button add_new_set = new Button("add new Set");
 		Button confirm = new Button("Confirm");
 		HBox buttonBox = new HBox(add_new_set,confirm);
-		VBox vBox = new VBox(hBox,buttonBox);
 		BorderPane pane = new BorderPane();
-		pane.setCenter(vBox);
 		Scene scene = new Scene(pane);
 		
+		pane.setCenter(hBox);
+		pane.setBottom(buttonBox);
 		stage.setScene(scene);
 		vBox.setSpacing(15);
 		vBox.setAlignment(Pos.CENTER);
