e711a593a2b7dbf1e2665f1f6f3aa3b61e9eedc4
Marcit2 <marcitrenki@gmail.com>
2022-05-25 15:42:36 +0200

    edited Files

--- src/main/java/com/example/maemory/CardSetFunctions.java
+++ src/main/java/com/example/maemory/CardSetFunctions.java
@@ -218,8 +218,9 @@ public class CardSetFunctions {
 		Button confirm = new Button("Confirm");
 		HBox buttonBox = new HBox(add_new_set,confirm);
 		BorderPane pane = new BorderPane();
-		Scene scene = new Scene(pane,350,200);
+		Scene scene = new Scene(pane,350,210);
 		
+		stage.setMinHeight(200);
 		stage.setMinWidth(350);
 		stage.setMaxWidth(500);
 		stage.setTitle("CardSets");
